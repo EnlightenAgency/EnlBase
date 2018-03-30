339c5b822c65560fd6e019f4419778b0026f28ac
Damain Stron <damian.strong@perficient.com>
2018-03-30 12:27:11 -0400

    compile scss, watch and dev server tasks

--- src/js/ENL.base.js
+++ src/js/ENL.base.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 	ENL = ENL || {};
 	ENL.utils = ENL.utils || {};
 	ENL.components = ENL.components || {};
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,62 @@
 const path = require('path');
 var glob = require("glob");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				cache: true,
+				parallel: true,
+				sourceMap: true // set to true if you want JS source maps
+			})
+		],
+		splitChunks: {
+			cacheGroups: {
+				styles: {
+					name: 'styles',
+					test: /\.scss$/,
+					chunks: 'all',
+					enforce: true
+				}
+			}
+		}
+	},
 	entry: {
 		scripts: ['./src/js/ENL.init.js', './src/js/ENL.base.js'],
 		vendors: glob.sync("./src/js/vendors/**/*.js")
 	},
 	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dest/js')
-	}
+		filename: './js/[name].bundle.js',
+		path: path.resolve(__dirname, 'dest')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader'
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+				MiniCssExtractPlugin.loader,
+				'css-loader',
+				'sass-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "./css/[name].bundle.css",
+		}),
+		new CopyWebpackPlugin([
+			{ from: './src/index.html', to: 'index.html' }
+		])
+	]
 };
